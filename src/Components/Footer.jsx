0ca6d205079b99a9b0217d41d778b5e2a5c10d03
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-07-07 16:12:10 +0530

    Create social connection footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import { FaFacebookF, FaTwitter, FaGoogle, FaInstagram, FaLinkedinIn, FaGithub, FaAngleUp } from 'react-icons/fa';
+
+const Footer = () => {
+    return (
+        <>
+            <style jsx='true'>
+                {`
+                     .btn {
+                        text-decoration: none !important;
+                        outline: 0 !important;
+                        box-shadow: none !important;
+                        border: none !important;
+                    }
+                `}
+            </style>
+
+            <footer className="text-center text-white" style={{ backgroundColor: "rgb(199, 199, 199)" }}>
+                <div className="container p-4">
+                    <h4 className='text-dark'>Connect with me on My Social Accounts</h4>
+                    <section className="mb-4">
+                        <a
+                            className="btn btn-primary m-1"
+                            style={{ backgroundColor: "#3b5998" }}
+                            href="https://www.facebook.com/bhavya.khurana.399"
+                            target="_blank"
+                            rel="noreferrer"
+                            role="button"
+                        >
+                            <FaFacebookF />
+                        </a>
+
+                        <a
+                            className="btn btn-primary btn-floating m-1"
+                            style={{ backgroundColor: "#55acee" }}
+                            href="https://twitter.com/Bhavya06001699"
+                            target="_blank"
+                            rel="noreferrer"
+                            role="button"
+                        >
+                            <FaTwitter />
+                        </a>
+
+                        <a
+                            className="btn btn-primary btn-floating m-1"
+                            style={{ backgroundColor: "#dd4b39" }}
+                            href="mailto:khuranabhavya24@gmail.com"
+                            target="_blank"
+                            rel="noreferrer"
+                            role="button"
+                        >
+                            <FaGoogle />
+                        </a>
+
+                        <a
+                            className="btn btn-primary btn-floating m-1"
+                            style={{ backgroundColor: "#ac2bac" }}
+                            href="https://www.instagram.com/__khurana__._/"
+                            target="_blank"
+                            rel="noreferrer"
+                            role="button"
+                        >
+                            <FaInstagram />
+                        </a>
+
+                        <a
+                            className="btn btn-primary btn-floating m-1"
+                            style={{ backgroundColor: "#0082ca" }}
+                            href="https://www.linkedin.com/in/bhavyakhurana24/"
+                            target="_blank"
+                            rel="noreferrer"
+                            role="button"
+                        >
+                            <FaLinkedinIn />
+                        </a>
+                        <a
+                            className="btn btn-primary btn-floating m-1"
+                            style={{ backgroundColor: "#333333" }}
+                            href="https://github.com/TheNewC0der-24"
+                            target="_blank"
+                            rel="noreferrer"
+                            role="button"
+                        >
+                            <FaGithub />
+                        </a>
+                    </section>
+                </div>
+
+                <div className="text-center p-3" style={{ backgroundColor: '#455a64' }}>
+                    © 2022 Copyright - <span className='badge text-bg-dark'>Bhavya Khurana</span>
+                </div>
+            </footer>
+        </>
+    );
+}
+
+export default Footer;
